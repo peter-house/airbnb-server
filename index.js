329fa3edc8228d27a48a513115c9ee228b13fb9b
김은총 <eunchong.k153@gmail.com>
2021-05-14 22:55:41 +0900

    Add index-js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const nearPlase = require("./routers/nearPlace");
+
+app.use('nearPlase', nearPlase);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
